21be7e7cc9cddc613f83613fb6bc1b4f29bd9ab1
Dixon Yant <dixonyant.dev@gmail.com>
2018-11-02 17:17:50 -0700

    Adding initial Firebase login with email and password

--- stc_game/Assets/Scripts/AuthManager.cs
+++ stc_game/Assets/Scripts/AuthManager.cs
@@ -1,16 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Firebase;
+using Firebase.Auth;
+using System.Threading.Tasks;
 
 public class AuthManager : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    //Firebase API variables
+    Firebase.Auth.FirebaseAuth auth;
+
+    public delegate IEnumerator AuthCallback(Task<Firebase.Auth.FirebaseUser> task, string operation);
+    public event AuthCallback authCallback;
+    void Awake() {
+        auth = FirebaseAuth.DefaultInstance;
+    }
+
+    public void SignUpNewUserWithEmailAndPassword(string email, string password) {
+        auth.CreateUserWithEmailAndPasswordAsync(email, password).ContinueWith(task => {
+            StartCoroutine(authCallback(task, "sign_up"));
+        });
+    }
+
+    public void LoginExistingUser(string email, string password) {
+        auth.SignInWithEmailAndPasswordAsync(email, password).ContinueWith (task => {
+            StartCoroutine(authCallback(task, "login"));
+        });
+    }
 }
---
+++ stc_game/Assets/Scripts/FormManager.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+
+public class FormManager : MonoBehaviour {
+
+	public InputField emailInput;
+	public InputField passwordInput;
+
+	public Button signUpButton;
+	public Button loginButton;
+	private AuthManager authManager;
+	
+	void Awake() {
+		ToggleButtonStates(false);
+
+		//Auth delegate subcriptions
+		authManager.authCallback += HandleAuthCallback;
+	}
+
+	public void ValidateEmail() {
+		string email = emailInput.text;
+		var regexPattern = @"^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$@";
+		if (email != "" && Regex.IsMatch(email, regexPattern) {
+			ToggleButtonStates(true);
+		} else {
+			ToggleButtonStates (false);
+		}
+	}
+
+	public void OnSignUp() {
+		authManager.SignUpNewUserWithEmailAndPassword(emailInput.text, passwordInput.text);
+		Debug.Log("Sign Up");
+	}
+
+	public void OnLogin() {
+		authManager.LoginExistingUser(emailInput.text, passwordInput.text);
+		Debug.Log("Login");
+	}
+
+	IEnumerator HandleAuthCallback (Task<Firebase.Auth.FirebaseUser> task, string operation) {
+		if (task.IsFaulted || task.IsCanceled) {
+			Debug.LogError("Sorry, an error happened creating your account, ERROR: " + task.Exception);
+		} else if (task.IsCompleted) {
+			Firebase.Auth.FirebaseUser newPlayer = task.Result;
+			Debug.LogFormat("Welcome! {0}", newPlayer.Email);
+			yield return true;
+		}
+	}
+
+	void onDestroy() {
+		authManager.authCallback -= HandleAuthCallback;
+	}
+
+	private void ToggleButtonStates(bool toState) {
+		signUpButton.interactable = toState;
+		loginButton.interactable = toState;
+	}
+}
