e97a39769bdbd1e9c71bdd9b369f1018f5280c22
Dixon Yant <dixonyant.dev@gmail.com>
2019-01-22 13:58:08 -0800

    deleting sample scene

---
+++ stc_game/Assets/Scripts/SceneControl/SceneSwitcher.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SceneSwitcher : MonoBehaviour
+{
+    public Button switchScene;
+
+    public void Awake()
+    {
+        switchScene.onClick.AddListener(GoToLoginScene);
+    }
+
+    public void GoToLoginScene()
+    {
+        SceneManager.LoadScene("TrainingGrounds");
+    }
+
+    //Can create simple loads for different scenes
+    //Makes this script a kind of master-key to get to scenes
+}
