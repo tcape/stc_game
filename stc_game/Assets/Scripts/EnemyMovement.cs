b6313f9ab1b4262dbaaa63506fcdf9eb3550ef43
TimCapehart <tcapehart@gmail.com>
2019-01-28 14:13:15 -0800

    Added more hallways and rooms
    Added more hallways and rooms. Still needs decoration and lighting

--- stc_game/Assets/Scripts/EnemyMovement.cs
+++ stc_game/Assets/Scripts/EnemyMovement.cs
@@ -11,90 +11,23 @@ namespace Assets.ImportedAssets.Warrior_Pack_Bundle_2.Code
 {
     class EnemyMovement : MonoBehaviour
     {
-        
-        [HideInInspector]
-        public Animator animator;
         public Warrior warrior;
-        private IKHands ikhands;
-        Rigidbody rigidBody;
-        public GameObject target;
-
-        private Vector3 startPosition;
+        public GameObject squadSergent;
         public float aggroDistance;
         public float stopFollowDistance;
         public float stopDistance = 2f;
-        public bool aggroTriggered = false;
-        public bool aggro;
-        public Vector3 distanceToTarget;
-        public NavMeshAgent agent;
-        float rotationSpeed = 15f;
+        public float rotationSpeed = 15f;
 
-        //public GameObject weaponModel;
-        //public GameObject secondaryWeaponModel;
+        [HideInInspector] public Animator animator;
+        [HideInInspector] Rigidbody rigidBody;
+        [HideInInspector] public GameObject target;
+        [HideInInspector] public bool aggroTriggered = false;
+        [HideInInspector] public bool aggro;
+        [HideInInspector] public Vector3 distanceToTarget;
+        [HideInInspector] public NavMeshAgent agent;
+        [HideInInspector] public bool specialAttack2Bool;
 
-        //public float gravity = -9.83f;
-        //public float runSpeed = 8f;
-        //public float runAnimMultiplier = 1f;
-        //public float walkSpeed = 3f;
-        //public float strafeSpeed = 3f;
-        //bool canMove = true;
-        ////jumping variables
-        //public float jumpSpeed = 8f;
-        //bool jumpHold = false;
-        //[HideInInspector]
-        //public bool canJump = true;
-        //float fallingVelocity = -2;
-        //bool isFalling = false;
-        //// Used for continuing momentum while in air
-        //public float inAirSpeed = 8f;
-        //float maxVelocity = 2f;
-        //float minVelocity = -2f;
-        //[HideInInspector]
-        //public Vector3 newVelocity;
-        //Vector3 inputVec;
-        //Vector3 dashInputVec;
-        //Vector3 targetDirection;
-        //bool isDashing = false;
-        //[HideInInspector]
-        //public bool isGrounded = true;
-        //[HideInInspector]
-        //public bool dead = false;
-        //bool isStrafing;
-        //[HideInInspector]
-        //public bool isAiming;
-        //bool aimingGui;
-        //[HideInInspector]
-        //public bool isBlocking = false;
-        //[HideInInspector]
-        //public bool isStunned = false;
-        //[HideInInspector]
-        //public bool isSitting = false;
-        //[HideInInspector]
-        //public bool inBlock;
-        //[HideInInspector]
-        //public bool blockGui;
-        //[HideInInspector]
-        //public bool weaponSheathed;
-        //[HideInInspector]
-        //public bool weaponSheathed2;
-        //bool isInAir;
-        //[HideInInspector]
-        //public bool isStealth;
-        //public float stealthSpeed;
-        //[HideInInspector]
-        //public bool isWall;
-        //[HideInInspector]
-        //public bool ledgeGui;
-        //[HideInInspector]
-        //public bool ledge;
-        //public float ledgeSpeed;
-        //[HideInInspector]
-        //public int attack = 0;
-        //bool canChain;
-        [HideInInspector]
-        public bool specialAttack2Bool;
-
-
+        private Vector3 startPosition;
         private Vector3 previousPosition;
         
 
@@ -103,40 +36,28 @@ namespace Assets.ImportedAssets.Warrior_Pack_Bundle_2.Code
         {
             animator = GetComponent<Animator>();
             rigidBody = GetComponent<Rigidbody>();
+            agent = GetComponent<NavMeshAgent>();
             target = GameObject.FindGameObjectWithTag("Player");
             startPosition = transform.position;
             aggroTriggered = false;
             aggro = false;
             animator.SetFloat("Speed", 0f);
-            //animator.SetInteger("Attack", 0);
-
         }
 
         private void Update()
         {
-            //previousPosition = transform.position;
             UpdateAggro();
         }
 
         private void FixedUpdate()
         {
             UpdateMovement();
-            //var speed = (float)Math.Sqrt(rigidBody.velocity.x * rigidBody.velocity.x + rigidBody.velocity.z * rigidBody.velocity.z);
             var speed = agent.velocity.magnitude;
             animator.SetFloat("Speed", speed);
-            
-
         }
 
-        private void LateUpdate() // Update animator values here
+        private void LateUpdate() // Update animator values here, or not?
         {
-            //rigidBody.velocity = (previousPosition - transform.position) / Time.deltaTime;
-
-
-            //float velocityXel = Math.Abs(rigidBody.velocity.x);
-            //float velocityZel = Math.Abs(rigidBody.velocity.z);
-            //animator.SetFloat("Input X", velocityXel);
-            //animator.SetFloat("Input Z", velocityZel);
 
         }
 
@@ -163,6 +84,18 @@ namespace Assets.ImportedAssets.Warrior_Pack_Bundle_2.Code
                 aggro = true;
                 aggroTriggered = true;
             }
+
+            if (squadSergent.Equals(null))
+            {
+                return;
+            }
+
+            if (squadSergent.GetComponent<EnemyMovement>().aggro)
+            {
+                aggroTriggered = true;
+                aggro = true;
+            }
+
            
         }
 
@@ -174,9 +107,6 @@ namespace Assets.ImportedAssets.Warrior_Pack_Bundle_2.Code
                 var distance = Math.Abs(Vector3.Distance(transform.position, target.transform.position));
                 if (distance > stopDistance)
                 {
-                    //transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(target.transform.position - transform.position), rotationSpeed * Time.deltaTime);
-                    //transform.position += transform.forward * runSpeed * Time.deltaTime;
-
                     agent.SetDestination(target.transform.position);
                     
                     animator.SetBool("Moving", true);
@@ -205,8 +135,6 @@ namespace Assets.ImportedAssets.Warrior_Pack_Bundle_2.Code
                     //    animator.SetInteger("Attack", 1);
                     //    return;
                     //}
-
-                   
                 }
                 Vector3 deltaVec = target.transform.position - transform.position;
                 transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(deltaVec), Time.deltaTime * rotationSpeed);
@@ -231,13 +159,7 @@ namespace Assets.ImportedAssets.Warrior_Pack_Bundle_2.Code
             agent.stoppingDistance = 0.5f;
             if (distance > agent.stoppingDistance)
             {
-
-                //transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(startPosition - transform.position), rotationSpeed * Time.deltaTime);
-                //transform.position += transform.forward * runSpeed * Time.deltaTime;
-
                 agent.SetDestination(startPosition);
-
-
                 animator.SetBool("Moving", true);
                 animator.SetBool("Running", true);
             }
