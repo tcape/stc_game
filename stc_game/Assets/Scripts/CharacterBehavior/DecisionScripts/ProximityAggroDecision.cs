863f1e704781f5e50274c4dda01402a490a15648
Dixon Yant <dixonyant.dev@gmail.com>
2019-02-02 13:49:14 -0800

    Reorganize file structure

---
+++ stc_game/Assets/Scripts/CharacterBehavior/DecisionScripts/ProximityAggroDecision.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu (menuName ="PluggableAI/Decisions/ProximityAggro")]
+public class ProximityAggro : Decision
+{
+    private bool aggro = false;
+
+    public override bool Decide(StateController controller)
+    {
+        return  Proximity(controller);
+    }
+
+    private bool Proximity(StateController controller)
+    {
+        var distance = Math.Abs(Vector3.Distance(controller.transform.position, controller.target.transform.position));
+
+        if (distance < controller.stats.aggroDistance)
+        {
+            aggro = true;
+        }
+
+        else
+        {
+            aggro = false;
+        }
+
+        return aggro;
+    }
+}
