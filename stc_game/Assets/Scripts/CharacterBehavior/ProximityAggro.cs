4c386486992849bdf7b90fa505577cbb35340f54
TimCapehart <tcapehart@gmail.com>
2019-02-01 21:33:57 -0800

    Began implementation of StateController
    Implemented StateConroller, State, Decision, Action, Transition, BehaviorStats, ChaseAction, PatrolAction, ProximityAggro, ReturnToStartPosition, StopChasingDecision, StopPatrolDecision

---
+++ stc_game/Assets/Scripts/CharacterBehavior/ProximityAggro.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu (menuName ="PluggableAI/Decisions/ProximityAggro")]
+public class ProximityAggro : Decision
+{
+    private bool aggro = false;
+
+    public override bool Decide(StateController controller)
+    {
+        aggro = Proximity(controller);
+        return aggro;
+    }
+
+    private bool Proximity(StateController controller)
+    {
+        var distance = Math.Abs(Vector3.Distance(controller.transform.position, controller.target.transform.position));
+
+        if (distance < controller.stats.aggroDistance)
+        {
+            aggro = true;
+        }
+
+        else
+        {
+            aggro = false;
+        }
+
+        return aggro;
+    }
+}
