d598f4fd4ece7f216c7a016f772f49c3ca4a1461
Dixon Yant <dixonyant.dev@gmail.com>
2019-02-19 10:52:00 -0800

    switches scene to dungeon from town

---
+++ stc_game/Assets/Scripts/SceneControl/TownScene/TriggerToDungeon.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TriggerToDungeon : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        SceneManager.LoadSceneAsync("Dungeon1");
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        
+    }
+}
