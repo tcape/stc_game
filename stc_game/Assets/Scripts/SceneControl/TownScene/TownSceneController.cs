68917288bd9e86ee117936e4245fe44b96dd2c23
Dixon Yant <dixonyant.dev@gmail.com>
2019-02-23 12:10:28 -0800

    more more naming changes

--- stc_game/Assets/Scripts/SceneControl/TownScene/TownSceneController.cs
+++ stc_game/Assets/Scripts/SceneControl/TownScene/TownSceneController.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
-public class TriggerToDungeon : MonoBehaviour
+public class TownSceneController : MonoBehaviour
 {
     public GameObject loadingScreen;
     public Slider slider;
