567a50d8369372a671cffd5b0ec6191c063dc771
Unknown <shantahuja@gmail.com>
2019-02-23 13:18:08 -0800

    wip gamestrings.cs
    gamestrings .cs creation

---
+++ stc_game/Assets/Scripts/GameStrings/GameStrings.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Scripts.GameStrings
+{
+    public class GameStrings
+    {
+        public GameStrings Instance
+        {
+            get
+            {
+                if (this == null)
+                {
+                    return new GameStrings();
+                }
+                else
+                {
+                    return this;
+                }
+            }
+            private set
+            {
+                Instance = this;
+            }
+        }
+
+        string TownScene = "Town";
+        string DungeonScene = "Dungeon";
+        string LoginScene = "Login";
+        string PersistentScene = "PersistentScene";
+
+    }
+}
