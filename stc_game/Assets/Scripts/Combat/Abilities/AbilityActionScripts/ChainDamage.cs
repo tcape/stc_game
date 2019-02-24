43083ed7eaf4532d42172903207e15ef659f69ce
TimCapehart <tcapehart@gmail.com>
2019-02-24 12:50:52 -0800

    Created abilities, hitbox collision script
    Slow, Increase speed, chain damage, hitbox collision script for enemy prefabs

---
+++ stc_game/Assets/Scripts/Combat/Abilities/AbilityActionScripts/ChainDamage.cs
@@ -0,0 +1,44 @@
+﻿using Assets.Scripts.CharacterBehavior.BaseClasses;
+using Assets.Scripts.CharacterBehavior.Combat;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Ability/AbilityAction/ChainDamage")]
+public class ChainDamage : AbilityAction
+{
+    private void OnEnable()
+    {
+        lastTick = 0;
+        effectTotal = 0;
+    }
+
+    public override void Act(AbilityManager manager)
+    {
+        target.GetComponent<CharacterStats>().TakeDamage(amount);
+
+        //if target's collider is touching others, they take damage too
+        foreach (var enemy in target.GetComponent<HitboxCollision>().touching)
+        {
+            enemy.GetComponent<CharacterStats>().TakeDamage(amount);
+            // and enemies they touch
+            foreach (var e in enemy.GetComponent<HitboxCollision>().touching)
+                e.GetComponent<CharacterStats>().TakeDamage(amount);
+        }
+    }
+
+    public override void RemoveEffect(AbilityManager manager)
+    {
+        return;
+    }
+
+    public override void ResetEffectTotal()
+    {
+        return;
+    }
+
+    public override void UpdateEffectTotal()
+    {
+        return;
+    }
+}
---
+++ stc_game/Assets/Scripts/Combat/HitboxCollision.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HitboxCollision : MonoBehaviour
+{
+    public List<GameObject> touching = new List<GameObject>();
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!GetComponentInParent<CharacterStats>().dead && other.gameObject.tag.Equals("Enemy"))
+        {
+            touching.Add(other.gameObject);
+        }
+    }
+
+    //private void OnTriggerStay(Collider other)
+    //{
+    //    if (GetComponentInParent<CharacterStats>().dead || other.gameObject.GetComponent<CharacterStats>().dead)
+    //        touching.Remove(other.gameObject);
+    //}
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag.Equals("Enemy"))
+        {
+            if (touching.Contains(other.gameObject))
+            {
+                touching.Remove(other.gameObject);
+            }
+        }
+    }
+
+    //private void Update()
+    //{
+    //    if (GetComponentInParent<CharacterStats>().dead)
+    //        touching.Clear();
+    //}
+}
