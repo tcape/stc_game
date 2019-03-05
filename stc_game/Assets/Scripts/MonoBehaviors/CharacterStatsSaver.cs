db54583758284376f23423f55a77858d9b8778a5
TimCapehart <tcapehart@gmail.com>
2019-03-05 12:19:00 -0800

    characterstats saver wip
    characterstats saver wip

---
+++ stc_game/Assets/Scripts/MonoBehaviors/CharacterStatsSaver.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Scripts.MonoBehaviors
+{
+    public class CharacterStatsSaver : Saver
+    {
+
+        public CharacterStats characterStats;
+
+
+        protected override void Load()
+        {
+            CharacterStats stats = new CharacterStats();
+            if (saveData.Load(key, ref stats))
+                characterStats = stats;
+        }
+
+        protected override void Save()
+        {
+            saveData.Save(key, characterStats);
+        }
+
+        protected override string SetKey()
+        {
+            return characterStats.name + characterStats.GetType().FullName + uniqueIdentifier;
+        }
+    }
+}
--- stc_game/Assets/Scripts/ScriptableObjects/DataPersistence/SaveData.cs
+++ stc_game/Assets/Scripts/ScriptableObjects/DataPersistence/SaveData.cs
@@ -14,6 +14,59 @@ using System.Collections.Generic;
 [CreateAssetMenu]
 public class SaveData : ResettableScriptableObject
 {
+
+    [Serializable]
+    public class KeyValuePairLists<T>
+    {
+        public List<string> keys = new List<string>();      // The keys are unique identifiers for each element of data. 
+        public List<T> values = new List<T>();              // The values are the elements of data.
+
+
+        public void Clear()
+        {
+            keys.Clear();
+            values.Clear();
+        }
+
+
+        public void TrySetValue(string key, T value)
+        {
+            // Find the index of the keys and values based on the given key.
+            int index = keys.FindIndex(x => x == key);
+
+            // If the index is positive...
+            if (index > -1)
+            {
+                // ... set the value at that index to the given value.
+                values[index] = value;
+            }
+            else
+            {
+                // Otherwise add a new key and a new value to the collection.
+                keys.Add(key);
+                values.Add(value);
+            }
+        }
+
+
+        public bool TryGetValue(string key, ref T value)
+        {
+            // Find the index of the keys and values based on the given key.
+            int index = keys.FindIndex(x => x == key);
+
+            // If the index is positive...
+            if (index > -1)
+            {
+                // ... set the reference value to the value at that index and return that the value was found.
+                value = values[index];
+                return true;
+            }
+
+            // Otherwise, return that the value was not found.
+            return false;
+        }
+    }
+
     // These are collections for various different data types.
     public KeyValuePairLists<bool> boolKeyValuePairLists = new KeyValuePairLists<bool> ();
     public KeyValuePairLists<int> intKeyValuePairLists = new KeyValuePairLists<int>();
