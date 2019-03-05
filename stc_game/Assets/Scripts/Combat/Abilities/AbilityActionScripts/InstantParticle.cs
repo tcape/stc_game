5a848daf4664063c6a064ad6b869eabfed16b8e9
Unknown <heroaurazero@hotmail.com>
2019-03-05 15:20:57 -0800

    particle action
    Particle action plays it's attached particle
    Abilities can attach multiple particle actions for a complex particle effect.
    
    Known bug: Two buffs that use the same particle action will disrupt each other's particle effects. New particle actions should be created for each ability. The individual particle system prefabs should still be reusable.

---
+++ stc_game/Assets/Scripts/Combat/Abilities/AbilityActionScripts/InstantParticle.cs
@@ -0,0 +1,55 @@
+﻿using Assets.Scripts.CharacterBehavior.BaseClasses;
+using Assets.Scripts.CharacterBehavior.Combat;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu (menuName ="Ability/AbilityAction/ParticleAction")]
+public class InstantParticle : AbilityAction
+{
+    public ParticleSystem particlePrefab;
+    private ParticleSystem prefabInstance;
+    private bool notStarted = true;
+    private void OnEnable()
+    {
+        notStarted = true;
+        lastTick = 0;
+        effectTotal = 0;
+    }
+
+    private void OnDestroy()
+    {
+        Destroy(prefabInstance);
+    }
+
+    public override void Act(AbilityManager manager)
+    {
+        //spawn particle effect
+        if (notStarted)
+        {
+            prefabInstance = Instantiate(particlePrefab);
+            prefabInstance.transform.position = manager.transform.position;
+            prefabInstance.transform.parent = manager.transform;
+            notStarted = false;
+        }
+    }
+
+    public override void RemoveEffect(AbilityManager manager)
+    {
+        lastTick = 0;
+        effectTotal = 0;
+        //delete particle effect
+        Destroy(prefabInstance);
+        notStarted = true;
+    }
+
+    public override void ResetEffectTotal()
+    {
+        
+    }
+
+    public override void UpdateEffectTotal()
+    {
+        
+    }
+}
