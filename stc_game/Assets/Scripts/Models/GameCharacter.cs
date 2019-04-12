b5a5a1f07d52cf98e5f732617007a4bf8b3b8f7e
TimCapehart <tcapehart@gmail.com>
2019-04-12 13:49:54 -0700

    Hero, GameCharacter

---
+++ stc_game/Assets/Scripts/Models/GameCharacter.cs
@@ -0,0 +1,26 @@
+﻿using Devdog.QuestSystemPro;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public enum HeroClass
+{
+    Warrior,
+    Mage
+}
+
+[Serializable]
+public class GameCharacter : MonoBehaviour
+{
+    public string Name;
+    public HeroClass HeroClass;
+    public StatsPreset StatsPreset;
+    public List<Ability> Abilities;
+    //public List<Item> Items;
+    //public List<EquipableItem> Equipment;
+    public GameObject Prefab;
+    public QuestDatabase questDatabase;
+
+}
---
+++ stc_game/Assets/Scripts/Models/Hero.cs
@@ -0,0 +1,36 @@
+﻿using Assets.Scripts.CharacterBehavior.Combat;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class Hero : MonoBehaviour
+{
+    private GameCharacter gameCharacter;
+    public string heroName;
+    public HeroClass heroClass;
+    public CharacterStats characterStats;
+    //public Inventory inventory;
+    //public Equipment equipment;
+    public AbilityManager abilityManager;
+    public GameObject prefab;
+
+
+    private void Awake()
+    {
+        gameCharacter = FindObjectOfType<GameCharacter>();
+        if (!gameCharacter)
+        {
+            Debug.Log("Game character not loaded.");
+            return;
+        }
+        heroName = gameCharacter.Name;
+        heroClass = gameCharacter.HeroClass;
+        characterStats = GetComponent<CharacterStats>();
+        // inventory = GetComponent<Inventory>();
+        // equpment = GetCompnent<Equipment>();
+        abilityManager = GetComponent<AbilityManager>();
+        prefab = gameObject;
+    }
+}
