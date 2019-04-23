3d7d6112790a1c98befc3695589178fc332c0b93
TimCapehart <tcapehart@gmail.com>
2019-04-23 11:20:46 -0700

    tweak
    tweak

---
+++ stc_game/Assets/Scripts/HUD/CooldownController.cs
@@ -0,0 +1,33 @@
+﻿using Assets.Scripts.CharacterBehavior.Combat;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CooldownController : MonoBehaviour
+{
+    public Transform abilitySlot;
+    private Image cooldown;
+    private Ability ability;
+
+    private void Awake()
+    {
+        cooldown = GetComponent<Image>();
+        cooldown.fillAmount = 0;
+    }
+
+    private void OnGUI()
+    {
+        ability = abilitySlot.GetComponent<AbilitySlot>().ability;
+    }
+
+    private void Update()
+    {
+        if (ability && ability.startTime != 0f)
+        {
+            if (ability.startTime < Time.time)
+                cooldown.fillAmount = 1f - (Time.time - ability.startTime) / ability.cooldown;
+        }
+        
+    }
+}
