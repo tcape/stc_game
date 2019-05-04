958b60f70da6bf2d0768abff55a9c4075fc95b53
TimCapehart <tcapehart@gmail.com>
2019-05-04 12:33:59 -0700

    deleted comments

--- stc_game/Assets/Scripts/Combat/Abilities/AbilityActionScripts/BuffDefense.cs
+++ stc_game/Assets/Scripts/Combat/Abilities/AbilityActionScripts/BuffDefense.cs
@@ -12,22 +12,16 @@ public class BuffDefense : AbilityAction
         lastTick = 0;
         effectTotal = 0;
     }
+
     public override void Act(AbilityManager manager)
     {
         manager.stats.stats.strength.defense.AddModifier(new StatModifier(amount, ModType.Flat, this));
-
-        // this is just for testing out MainStat modifiers and their Substat mod effects
-        //manager.stats.stats.strength.AddStatModifier(new StatModifier(amount, ModType.Flat, this));
-
         UpdateEffectTotal();
     }
 
     public override void RemoveEffect(AbilityManager manager)
     {
         manager.stats.stats.strength.defense.RemoveAllModifiersFromSource(this);
-
-        // this is just for testing out MainStat modifiers and their Substat mod effects
-       // manager.stats.stats.strength.RemoveStatModifiersFromSource(this);
     }
 
     public override void ResetEffectTotal()
