bb958123fff2b46d627c35ea7cf86326156acb3b
Unknown <shantahuja@gmail.com>
2019-05-14 14:28:13 -0700

    collect gold quest
    #210

--- stc_game/Assets/Devdog/QuestSystemPro/Scripts/Quests/ProgressAffectors/SetQuestProgressOnTriggerObjectGold.cs
+++ stc_game/Assets/Devdog/QuestSystemPro/Scripts/Quests/ProgressAffectors/SetQuestProgressOnTriggerObjectGold.cs
@@ -2,12 +2,13 @@
 using System.Collections;
 using System;
 using Devdog.General;
+using Assets.Scripts.CharacterBehavior.Drops;
 
 namespace Devdog.QuestSystemPro
 {
     [AddComponentMenu(QuestSystemPro.AddComponentMenuPath + "Set Quest Progress/Set Quest Progress On Trigger Object")]
     [RequireComponent(typeof(Trigger))]
-    public sealed class SetQuestProgressOnTriggerObject : MonoBehaviour, ITriggerCallbacks
+    public sealed class SetQuestProgressOnTriggerObjectGold : MonoBehaviour, ITriggerCallbacks
     {
         private enum Use
         {
@@ -15,31 +16,41 @@ namespace Devdog.QuestSystemPro
             OnUnUse
         }
 
+        public Gold gold;
 
+        public void Start()
+        {
+            gold = gameObject.GetComponent<Gold>();
+        }
 
         public QuestProgressDecorator progress;
+
 
         [Header("Trigger configuration")]
         [SerializeField]
         private Use _use;
 
 
-        public  bool OnTriggerUsed(Player player)
+        public bool OnTriggerUsed(Player player)
         {
             if (_use == Use.OnUse)
             {
+                progress.progress += (float)gold.amount;
                 progress.Execute();
-                
+
+
             }
 
             return false;
         }
 
-        public  bool OnTriggerUnUsed(Player player)
+        public bool OnTriggerUnUsed(Player player)
         {
             if (_use == Use.OnUnUse)
             {
+                progress.progress += (float)gold.amount;
                 progress.Execute();
+
             }
 
             return false;
