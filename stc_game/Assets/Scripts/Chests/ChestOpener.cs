0be3515db29949c0509ca56a3de2cbc128c4d6b0
TimCapehart <tcapehart@gmail.com>
2019-05-17 13:57:18 -0700

    WIP

--- stc_game/Assets/Scripts/Chests/ChestOpener.cs
+++ stc_game/Assets/Scripts/Chests/ChestOpener.cs
@@ -1,11 +1,16 @@
-﻿using System.Collections;
+﻿using Assets.Scripts.CharacterBehavior.Drops;
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class ChestOpener : MonoBehaviour
 {
     private Camera cam;
     private Animator animator;
+    private bool opened;
+    public double goldAmount;
 
     // Start is called before the first frame update
     void Start()
@@ -13,25 +18,53 @@ public class ChestOpener : MonoBehaviour
         cam = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
         animator = GetComponent<Animator>();
         animator.SetBool("open", false);
+        opened = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButton(1))
+        if (Input.GetMouseButtonDown(1))
         {
-            // raycast at mouse position
-            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
-            RaycastHit hit;
+            OpenAndCloseChest();
+        }
+    }
 
-            // if hit something get the hit
-            if (Physics.Raycast(ray, out hit))
+    private void OpenAndCloseChest()
+    {
+        // raycast at mouse position
+        Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        // if hit something get the hit
+        if (Physics.Raycast(ray, out hit))
+        {
+            if (hit.collider == gameObject.GetComponent<BoxCollider>())
             {
-                if (hit.collider == GetComponent<MeshCollider>() || GetComponentInChildren<MeshCollider>())
+                if (!animator.GetBool("open"))
                 {
                     animator.SetBool("open", true);
+                    if (!opened)
+                    {
+                        // instatiate stuff
+                        StartCoroutine(SpawnGold());
+                        opened = true;
+                    }
+                }
+                else if (animator.GetBool("open"))
+                {
+                    animator.SetBool("open", false);
                 }
             }
         }
+    }
+    
+    private IEnumerator SpawnGold()
+    {
+        yield return new WaitForSeconds(1f);
+        var gold = Instantiate(Resources.Load("Prefabs/Gold1"), gameObject.transform) as GameObject;
+        gold.transform.rotation = transform.rotation;
+        gold.transform.position = transform.position + new Vector3(0, 0, -1.3f); // need to get this to make gold appear in front of chest everytime
+        gold.GetComponent<Gold>().SetAmount(goldAmount);
     }
 }
