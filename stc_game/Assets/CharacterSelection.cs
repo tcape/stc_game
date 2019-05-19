1fd4cc6d6b5436a9d1f42e1527e7ba3d629b2c94
TimCapehart <tcapehart@gmail.com>
2019-05-19 11:22:24 -0700

    Got fade working, made a warrior entrance

--- stc_game/Assets/CharacterSelection.cs
+++ stc_game/Assets/CharacterSelection.cs
@@ -11,7 +11,6 @@ public class CharacterSelection : MonoBehaviour
     public GameObject warrior;
     private GameObject activeCharacter;
 
-    // Start is called before the first frame update
     void Start()
     {
         mageButton.onClick.AddListener(OnClickMageButton);
@@ -24,18 +23,37 @@ public class CharacterSelection : MonoBehaviour
         activeCharacter.SetActive(true);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void SetMaterialToTransparentFade(Material material)
     {
-        
+        material.SetFloat("_Mode", 2);
+        material.SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.SrcAlpha);
+        material.SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.OneMinusSrcAlpha);
+        material.SetInt("_ZWrite", 0);
+        material.DisableKeyword("_ALPHATEST_ON");
+        material.EnableKeyword("_ALPHABLEND_ON");
+        material.DisableKeyword("_ALPHAPREMULTIPLY_ON");
+        material.renderQueue = 3000;
     }
 
-    IEnumerator FadeMage(MeshRenderer meshRender, float lerpDuration)
+    private void SetMaterialToOpaque(Material material)
     {
-        var meshColor = meshRender.material.color;
+        material.SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.One);
+        material.SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.Zero);
+        material.SetInt("_ZWrite", 1);
+        material.DisableKeyword("_ALPHATEST_ON");
+        material.EnableKeyword("_ALPHABLEND_ON");
+        material.DisableKeyword("_ALPHAPREMULTIPLY_ON");
+        material.renderQueue = -1;
+    }
+
+    IEnumerator FadeCharacter(Material material, float lerpDuration)
+    {
+        SetMaterialToTransparentFade(material);
+
+        var meshColor = material.color;
         Color startLerp = new Color(meshColor.r, meshColor.g, meshColor.b, 0f);
         Color targetLerp = new Color(meshColor.r, meshColor.g, meshColor.b, 1f);
-        meshRender.material.color = startLerp;
+        material.color = startLerp;
         activeCharacter.SetActive(true);
         float lerpStart_Time = Time.time;
         float lerpProgress;
@@ -44,9 +62,9 @@ public class CharacterSelection : MonoBehaviour
         {
             yield return new WaitForEndOfFrame();
             lerpProgress = Time.time - lerpStart_Time;
-            if (meshRender != null)
+            if (material != null)
             {
-                meshRender.material.color = Color.Lerp(startLerp, targetLerp, lerpProgress / lerpDuration);
+                material.color = Color.Lerp(startLerp, targetLerp, lerpProgress / lerpDuration);
             }
             else
             {
@@ -57,10 +75,12 @@ public class CharacterSelection : MonoBehaviour
                 lerping = false;
             }
         }
+
+        SetMaterialToOpaque(material);
         yield break;
     }
 
-    void OnClickMageButton()
+    public void OnClickMageButton()
     {
         if (activeCharacter == warrior)
         {
@@ -68,19 +88,35 @@ public class CharacterSelection : MonoBehaviour
             activeCharacter = mage;
             var teleportEffect = Instantiate(Resources.Load("FX/Mage_Entrance_Effect_2")) as GameObject;
             var poofEffect = Instantiate(Resources.Load("FX/Mage_Entrance_Burst")) as GameObject;
-            teleportEffect.transform.position = mage.transform.position + new Vector3(.5f, 1.8f, 0f);
-            poofEffect.transform.position = mage.transform.position + new Vector3(.1f, 1f, 0f);
-            StartCoroutine(FadeMage(mage.GetComponent<MeshRenderer>(), 2f));
+            teleportEffect.transform.position = mage.transform.position + new Vector3(.3f, 1.7f, 0f);
+            poofEffect.transform.position = mage.transform.position + new Vector3(.1f, 1.4f, 0f);
+            StartCoroutine(FadeCharacter(mage.GetComponentInChildren<SkinnedMeshRenderer>().material, 0.5f));
         }
     }
 
-    void OnClickWarriorButton()
+    public void OnClickWarriorButton()
     {
         if (activeCharacter == mage)
         {
             activeCharacter.SetActive(false);
             activeCharacter = warrior;
-            activeCharacter.SetActive(true);
+            var warriorEffectBurst = Instantiate(Resources.Load("FX/Warrior_Entrance_Burst")) as GameObject;
+            var warriorEntrance = Instantiate(Resources.Load("FX/Warrior_Entrance_Effect")) as GameObject;
+            warriorEffectBurst.transform.position = warrior.transform.position + new Vector3(0f, 1.2f, 0f);
+            warriorEntrance.transform.position = warrior.transform.position + new Vector3(-.2f, 1.2f, 0f);
+            StartCoroutine(FadeCharacter(warrior.GetComponentInChildren<SkinnedMeshRenderer>().material, 0.5f));
+            StartCoroutine(WarriorAttacks());
         }
+    }
+
+    private IEnumerator WarriorAttacks()
+    {
+        var animator = warrior.GetComponent<Animator>();
+        animator.SetBool("Attacking", true);
+        animator.SetBool("Moving", false);
+        animator.SetInteger("Attack", 1);
+        yield return new WaitForSeconds(.8f);
+        animator.SetInteger("Attack", 0);
+        animator.SetBool("Attacking", false);
     }
 }
