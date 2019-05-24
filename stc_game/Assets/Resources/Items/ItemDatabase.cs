d9aa8d2efb45c69781a150b317f2aeaf508c6c56
TimCapehart <tcapehart@gmail.com>
2019-05-24 12:25:06 -0700

    GameItemDB

---
+++ stc_game/Assets/Resources/Items/ItemDatabase.cs
@@ -0,0 +1,11 @@
+﻿using Kryz.CharacterStats.Examples;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class ItemDatabase : ScriptableObject
+{
+    public List<Item> items;
+
+}
